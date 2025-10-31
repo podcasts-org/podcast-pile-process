3361962817cb67a4199590f2b32abea901d4c6c6
mrfakename <me@mrfake.name>
2025-10-30 20:24:14 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="podcastpile",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=True,
